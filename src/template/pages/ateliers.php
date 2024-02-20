fc8f2e0c78d5a30dfebc25a81438941531555db3
Phearal <user.name>
2024-02-20 23:30:59 +0100

    feat : page ateliers OK

--- src/template/pages/ateliers.php
+++ src/template/pages/ateliers.php
@@ -6,13 +6,63 @@
     <section class="section-enfants">
         <h2>Pour les enfants</h2>
         <div class="separator"></div>
-        <article>
-            <div>
-
-                <p>Au cœur d'une amitié partagée et d'une passion commune pour l'art, un groupe d'amis décida de transformer un simple grenier en un atelier créatif. Ils baptisèrent leur projet "Corvus luminifère" et créèrent un espace où chaque coup de pinceau devint une histoire à raconter.
-                <br><br>Ainsi naquit un lieu où artistes novices et chevronnés se rassemblent, partagent et exposent. "Corvus luminifère" devint le sanctuaire où la passion artistique s'illumine, où chaque visiteur est invité à découvrir et nourrir sa propre créativité. Bienvenue dans notre histoire, devenez une part de "Corvus luminifère".</p>
-            </div>
-            <img class="img-default" src="<?= IMAGES ?>/expositions/expositions.jpg" alt="Exposition du 27 Février 2018, les visiteurs examinent les toiles.">
-        </article>
+        <ul>
+            <li>
+                <h3>Atelier de Peinture Acrylique</h3>
+                <p><b>Méthode</b> : Les enfants explorent la peinture acrylique en apprenant différentes techniques de base, telles que le mélange des couleurs, les effets de texture et les coups de pinceau.</p>
+                <p><b>Durée</b> : 3h</p>
+                <p><b>Tarif</b> : 30€</p>
+                <p>Cet atelier permet aux enfants de s'exprimer librement à travers la peinture. Ils peuvent créer des paysages colorés, des portraits imaginaires ou des compositions abstraites en utilisant la peinture acrylique sur toile ou papier.</p>
+            </li>
+            <figure>
+                <img class="img-default" src="<?= IMAGES ?>/activites/atelier2.jpg" alt="Exposition du 27 Février 2018, les visiteurs examinent les toiles.">
+                <figcaption class="hide">Découvrez nos ateliers pour adultes et explorez votre créativité.</figcaption>
+            </figure>
+            <li>
+                <h3>Cours de Dessin au Fusain</h3>
+                <p><b>Méthode</b> : Les enfants apprennent les bases du dessin au fusain en explorant les techniques de dessin à main levée, de hachures et de mise en valeur des contrastes.</p>
+                <p><b>Durée</b> : 1h30</p>
+                <p><b>Tarif</b> : 15€</p>
+                <p>Cet atelier encourage les enfants à expérimenter avec le fusain, un médium polyvalent idéal pour créer des dessins expressifs et dramatiques. Ils peuvent dessiner des natures mortes, des portraits ou des paysages en utilisant le fusain pour capturer les formes et les textures avec précision.</p>
+            </li>
+            <li>
+                <h3>Création de Bandes Dessinées</h3>
+                <p><b>Méthode</b> : Les enfants apprennent les bases de la création de bandes dessinées, y compris le développement de personnages, la mise en page des cases et la narration visuelle.</p>
+                <p><b>Durée</b> : 2h</p>
+                <p><b>Tarif</b> : 20€</p>
+                <p>Dans cet atelier, les enfants explorent l'art de la bande dessinée en créant leurs propres histoires illustrées. Ils développent leurs compétences en dessin, en écriture et en narration tout en laissant libre cours à leur imagination pour créer des aventures passionnantes et amusantes.</p>
+            </li>
+        </ul>
+    </section>
+    <section class="section-adultes">
+        <h2>Pour les adultes</h2>
+        <div class="separator"></div>
+        <ul>
+            <li>
+                <h3>Atelier de Peinture Acrylique</h3>
+                <p><b>Méthode</b> : Les participants explorent les techniques de peinture à l'acrylique, en apprenant à mélanger les couleurs, à créer des textures et à composer des compositions équilibrées.</p>
+                <p><b>Durée</b> : 3h</p>
+                <p><b>Tarif</b> : 30€</p>
+                <p>Cet atelier offre aux adultes la possibilité de s'exprimer à travers la peinture à l'acrylique. Ils peuvent choisir parmi une variété de sujets, tels que des natures mortes, des portraits ou des paysages, et expérimenter avec les techniques pour créer des œuvres originales et inspirantes.</p>
+            </li>
+            <figure>
+                <img class="img-default" src="<?= IMAGES ?>/activites/atelier3.jpg" alt="Exposition du 27 Février 2018, les visiteurs examinent les toiles.">
+                <figcaption class="hide">Découvrez nos ateliers pour adultes et explorez votre créativité.</figcaption>
+            </figure>
+            <li>
+                <h3>Atelier de Dessin au Fusain et au Graphite</h3>
+                <p><b>Méthode</b> : Les participants explorent les techniques de dessin au fusain et au graphite, en apprenant à travailler avec ces deux médiums pour créer des contrastes et des textures riches.</p>
+                <p><b>Durée</b> : 1h30</p>
+                <p><b>Tarif</b> : 15€</p>
+                <p>Cet atelier permet aux adultes d'expérimenter deux des médiums les plus classiques du dessin : le fusain et le graphite. Ils auront l'opportunité de découvrir les différentes nuances et les possibilités créatives offertes par ces matériaux polyvalents. Les participants apprendront à utiliser le fusain pour créer des lignes audacieuses et expressives, ainsi que le graphite pour des détails fins et des nuances subtiles. C'est une occasion idéale pour explorer les fondamentaux du dessin tout en jouant avec une variété d'outils artistiques.</p>
+            </li>
+            <li>
+                <h3>Session de Gravure sur Linoléum</h3>
+                <p><b>Méthode</b> : Les participants utilisent des outils de gravure pour créer des estampes sur des plaques de linoléum, en expérimentant avec les motifs, les textures et les effets d'impression.</p>
+                <p><b>Durée</b> : 3h</p>
+                <p><b>Tarif</b> : 50€</p>
+                <p>Dans cet atelier de gravure sur linoléum, les adultes peuvent découvrir les techniques de gravure traditionnelles pour créer des estampes uniques et expressives. Ils peuvent expérimenter avec les motifs et les designs pour créer des œuvres d'art originales à emporter chez eux.</p>
+            </li>
+        </ul>
     </section>
 </main>
