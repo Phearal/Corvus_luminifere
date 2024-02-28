959af2d10158c0b86ec4acfa7c9ad4b4b47537cb
Phearal <user.name>
2024-02-28 23:20:41 +0100

    refactor : label for p + title for links

--- src/template/pages/contact.php
+++ src/template/pages/contact.php
@@ -23,31 +23,31 @@
         <h2>Coordonnées</h2>
         <div class="separator"></div>
         <address>
-            <label>Adresse</label>
+            <p>Adresse</p>
             <p>19 rue des Pinsons, 64140 Lons</p>
-            <label>Email</label>
+            <p>Email</p>
             <p>corvusluminifere@contact.fr</p>
-            <label>Téléphone</label>
+            <p>Téléphone</p>
             <p>06.01.01.01.01</p>
         </address>
         <h3>Suivez nous sur nos réseaux</h3>
         <ul>
             <li>
-                <a href="https://www.facebook.com/" target="_blank">
+                <a href="https://www.facebook.com/" target="_blank" title="Visitez notre page Facebook">
                     <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6.27993 9.2H8.27993L9.07993 6H6.27993V4.4C6.27993 3.576 6.27993 2.8 7.87993 2.8H9.07993V0.112C8.81913 0.0776 7.83433 0 6.79433 0C4.62233 0 3.07993 1.3256 3.07993 3.76V6H0.679932V9.2H3.07993V16H6.27993V9.2Z" fill="white"/>
                     </svg>
                 </a>
             </li>
             <li>
-                <a href="https://twitter.com" target="_blank">
+                <a href="https://twitter.com" target="_blank" title="Visitez notre page Twitter">
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M16 16L9.743 6.67491L9.75367 6.68364L15.3953 0H13.51L8.91418 5.44L5.26456 0H0.320142L6.16167 8.70618L6.16096 8.70545L0 16H1.88528L6.99475 9.94764L11.0556 16H16ZM4.51756 1.45455L13.2966 14.5455H11.8026L3.01645 1.45455H4.51756Z" fill="white"/>
                     </svg>
                 </a>
             </li>
             <li>
-                <a href="https://www.instagram.com/" target="_blank">
+                <a href="https://www.instagram.com/" target="_blank" title="Visitez notre page Instagram">
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M4.64423 0C2.08654 0 0 2.08654 0 4.64423V11.3558C0 13.9135 2.08654 16 4.64423 16H11.3558C13.9135 16 16 13.9135 16 11.3558V4.64423C16 2.08654 13.9135 0 11.3558 0H4.64423ZM4.64423 1.23077H11.3558C13.2476 1.23077 14.7692 2.75 14.7692 4.64423V11.3558C14.7692 13.2476 13.25 14.7692 11.3558 14.7692H4.64423C2.7524 14.7692 1.23077 13.25 1.23077 11.3558V4.64423C1.23077 2.7524 2.75 1.23077 4.64423 1.23077ZM12.6154 2.46154C12.1058 2.46154 11.6923 2.875 11.6923 3.38462C11.6923 3.89423 12.1058 4.30769 12.6154 4.30769C13.125 4.30769 13.5385 3.89423 13.5385 3.38462C13.5385 2.875 13.125 2.46154 12.6154 2.46154ZM8 3.69231C5.6274 3.69231 3.69231 5.6274 3.69231 8C3.69231 10.3726 5.6274 12.3077 8 12.3077C10.3726 12.3077 12.3077 10.3726 12.3077 8C12.3077 5.6274 10.3726 3.69231 8 3.69231ZM8 4.92308C9.70673 4.92308 11.0769 6.29327 11.0769 8C11.0769 9.70673 9.70673 11.0769 8 11.0769C6.29327 11.0769 4.92308 9.70673 4.92308 8C4.92308 6.29327 6.29327 4.92308 8 4.92308Z" fill="white"/>
                     </svg>
